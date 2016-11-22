bca805c23e89eebc64827c0c9032a99d375afc68
omarsalazar <salazarpazomar@gmail.com>
2016-11-22 02:32:23 -0600

    Tengo que aprender a hacer pruebas :/

--- materia/tests.py
+++ materia/tests.py
@@ -7,12 +7,13 @@ from datetime import datetime
 class HorasTestCase(TestCase):
     def setUp(self):
         hoy = datetime.now()
-        usuario = User.objects.create_user(username = "Omar", password = "password")
+        usuario = User.objects.create_user(username = "Pepe", password = "password")
         semestret = semestre.objects.create(id_semestre = "1451", fecha_inicio = hoy,
         fecha_final = hoy, user = usuario)
         horario = datoshorario.objects.create(user = usuario, semestre = semestret)
+        #Datos ingresados por el usuario
         materia = datosmateria.objects.create(materia = "Orientacion", profesor = "Hilda Moreno",
-        grupo = "1234", horario = horario)
+        grupo = "6IV3", horario = horario)
 
     def test(self):
         materia = datosmateria.objects.get(materia = "Orientacion")
