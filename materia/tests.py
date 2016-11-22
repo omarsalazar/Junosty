6cbc3b504c1f36bb8ee534acafe73c581257c542
omarsalazar <salazarpazomar@gmail.com>
2016-11-22 02:27:48 -0600

    Prueba unitaria mal hecha xD

--- materia/tests.py
+++ materia/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your tests here.
+class HorasTestCase(TestCase):
+    def setUp(self):
+        hoy = datetime.now()
+        usuario = User.objects.create_user(username = "Omar", password = "password")
+        semestret = semestre.objects.create(id_semestre = "1451", fecha_inicio = hoy,
+        fecha_final = hoy, user = usuario)
+        horario = datoshorario.objects.create(user = usuario, semestre = semestret)
+        materia = datosmateria.objects.create(materia = "Orientacion", profesor = "Hilda Moreno",
+        grupo = "1234", horario = horario)
+
+    def test(self):
+        materia = datosmateria.objects.get(materia = "Orientacion")
+        return self.assertEqual(materia.materia, 'Matematicas')
