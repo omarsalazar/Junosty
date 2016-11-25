5c2afb3fc630706b86f5a2f93cdcf12c0b140801
LopsanAMO <makiki789@gmail.com>
2016-11-25 03:34:43 -0600

    ya se ve el horaio, soy la verga andante

--- semestre/views.py
+++ semestre/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import View
+from materia.models import datosmateria, horas, datoshorario
 
 
 class Horario(View):
     def get(self, request):
         template_name = 'horario.html'
-        return render(request, template_name)
+        horario = datoshorario.objects.filter(user=request.user)
+        materias = datosmateria.objects.filter(horario=horario)
+        mate = datosmateria.objects.values_list(
+            'id',
+            flat=True).filter(
+                horario=horario
+            )
+        dic = list(mate)
+        hora = horas.objects.filter(materia__in=dic)
+        context = {
+            'materita': materias,
+            'horitas': hora
+        }
+        return render(request, template_name, context)
